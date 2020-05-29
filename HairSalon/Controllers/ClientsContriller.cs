9f99da975dec0578a837c22778d98aaa026614a5
nitunwa <nitun.2@gmail.com>
2020-05-29 14:09:30 -0700

    successfully add client to the client list and display details of a client

--- HairSalon/Controllers/ClientsContriller.cs
+++ HairSalon/Controllers/ClientsContriller.cs
@@ -20,7 +20,24 @@ namespace HairSalon.Controllers
       List<Client> model = _db.Clients.ToList();
       return View(model);
     }
+    public ActionResult Create()
+    {
+      return View();
+    }
 
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+
+      return View(thisClient);
+    }
 
 
   }
