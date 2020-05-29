332b74ffb0492b857bb2da69363274780319eec1
nitunwa <nitun.2@gmail.com>
2020-05-29 11:47:30 -0700

    successfully navigate from Home page to create stylist form page and stylist details page

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -6,6 +6,7 @@ namespace HairSalon.Models
     public int StylistId { get; set; }
     public string StylistName { get; set; }
     public string StylistPhoneNum { get; set; }
+    public int Experince { get; set; }
     public virtual ICollection<Client> Clients { get; set; }
     public Stylist()
     {
